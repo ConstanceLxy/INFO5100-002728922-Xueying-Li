a8c9fb1bd1c3fde9e47bd2285004f6ed423b3542
ConstanceLxy <li.xueyin@Northeastern.edu>
2023-02-18 09:47:17 -0800

    exercise02

---
+++ exercise/exercise02/src/Square.java
@@ -0,0 +1,18 @@
+public class Square extends Shape {
+    private double side;
+
+    public Square(String color, double side) {
+        super(color);
+        this.side = side;
+    }
+
+    @Override
+    public double calculateArea() {
+        return side * side;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 4 * side;
+    }
+}
